99cb8a86a9fbdbc98a3243c79e78bc392e08fb01
Kayden Drake <root@localhost.localdomain>
2019-08-15 12:09:35 -0600

    Adds potluck code to add a dish

--- lib/potluck.rb
+++ lib/potluck.rb
@@ -3,8 +3,13 @@ require './lib/dish.rb'
 class Potluck
   attr_reader :date
   attr_reader :dishes
+
   def initialize(potluckDate)
     @date = potluckDate
     @dishes = []
+  end
+
+  def add_dish(dish)
+    @dishes.push(dish)
   end
 end
