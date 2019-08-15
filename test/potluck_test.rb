407da3b77d96d65af8a931a7c07c84893536fc82
Kayden Drake <root@localhost.localdomain>
2019-08-15 12:19:01 -0600

    Adds test for adding multiple potluck dishes

--- test/potluck_test.rb
+++ test/potluck_test.rb
@@ -11,16 +11,19 @@ class PotluckTest < Minitest::Test
     potluck = Potluck.new("8-21-19")
     assert_instance_of Potluck, potluck
   end
+
   #test it has a date
   def test_potluck_has_a_date
     potluck = Potluck.new("8-21-19")
     assert_equal "8-21-19", potluck.date
   end
+
   #test it has a list of dishes
   def test_potluck_has_a_list_of_dishes
     potluck = Potluck.new("8-21-19")
     assert_equal [], potluck.dishes
   end
+
   #test it can add a dish
   def test_potluck_can_add_a_dish
     potluck = Potluck.new("8-21-19")
@@ -30,5 +33,17 @@ class PotluckTest < Minitest::Test
 
     assert_equal bean_dip, potluck.dishes[0]
   end
+
   #test it can add multiple dishes
+  def test_potluck_can_add_multiple_dishes
+    potluck = Potluck.new("8-21-19")
+    bean_dip = Dish.new("Bean Dip", :appetizer)
+    pulled_pork = Dish.new("Pulled Pork", :entre)
+
+    potluck.add_dish(bean_dip)
+    potluck.add_dish(pulled_pork)
+
+    assert_equal bean_dip, potluck.dishes[0]
+    assert_equal pulled_pork, potluck.dishes[1]
+  end
 end
